744e816950301c51123badfe8ed3c4454b2eab7f
thomas <makis7olymp@yahoo.gr>
2023-07-02 18:43:54 +0300

    49-fetch-data-for-home-page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import Content from '../features/ui/content';
 import { Typography } from 'antd';
 import CreateCard from '../features/components/create-card';
 import { cardsData } from '../shared/setup/home';
+import { fetchProducts } from '../redux/slices/products';
+import { fetchCustomers } from '../redux/slices/customers';
+import { fetchOrders } from '../redux/slices/orders';
 
 const CardWrapper = styled.div`
   width: 100%;
@@ -24,6 +28,27 @@ const CardWrapper = styled.div`
 const { Title } = Typography;
 
 function Home() {
+  const dispatch = useDispatch();
+  const actions = [fetchCustomers(), fetchOrders(), fetchProducts()];
+  const orders = useSelector((state) => state.orders.data);
+  const customers = useSelector((state) => state.customers.data);
+  const products = useSelector((state) => state.products.data);
+
+  const getValue = (index) => {
+    switch (index) {
+      case 0:
+        return customers?.length;
+      case 1:
+        return orders?.length;
+      default:
+        return products?.length;
+    }
+  };
+
+  useEffect(() => {
+    actions?.map((action) => dispatch(action));
+  }, []);
+
   return (
     <Content>
       <Title style={{ color: 'rgb(17, 25, 39)' }} level={2}>
@@ -31,7 +56,7 @@ function Home() {
       </Title>
       <CardWrapper>
         {cardsData?.map((card, index) => (
-          <CreateCard {...card} key={index} />
+          <CreateCard {...card} key={index} value={getValue(index)} />
         ))}
       </CardWrapper>
     </Content>
